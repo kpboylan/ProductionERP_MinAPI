ff10df4887505e34d433cf2423d8bddcdcbf7955
kpboylan <kboylan@gmail.com>
2025-06-13 10:37:57 +0100

    Change to DateTimeOffset

--- ProductionERP_MinAPI/Model/Material.cs
+++ ProductionERP_MinAPI/Model/Material.cs
@@ -15,6 +15,6 @@
 
         public string? CountryCode { get; set; } = "IE";
         public int ManufacturerID { get; set; }
-        public DateTime Expiration { get; set; }
+        public DateTimeOffset Expiration { get; set; }
     }
 }
